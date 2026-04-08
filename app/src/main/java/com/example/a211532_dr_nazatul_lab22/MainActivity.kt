c8584bf27770fa162bde29efdaaeeedda6b521e4
a211532-dot <a211532@siswa.edu.my>
2026-04-08 09:26:03 +0800

    Add new feature

--- app/src/main/java/com/example/a211532_dr_nazatul_lab22/MainActivity.kt
+++ app/src/main/java/com/example/a211532_dr_nazatul_lab22/MainActivity.kt
@@ -112,7 +112,7 @@ fun HomeScreenLayout() {
                         )
                     )
 
-                    // butto n
+                    // button
                     Button(
                         onClick = {
 
